db6b0d4eeaf8f7e0cdc338d6dd7dfa2a8af315e8
Richard Huang <flyerhzm@gmail.com>
2012-02-05 21:36:14 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsProxy::Application.config.secret_token = '1fd1c4461dcbc5dec08c1eb32b4ec4478149ef8f3d6b599be85b81527ea744bc12cb70943e48880a85b5fe9b48af75598bb195ddfa68107b522d267fc5208fb8'
